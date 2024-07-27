849566d872a5060782783c661a5645ff99f7e731
Ruler <148696064+quehole@users.noreply.github.com>
2024-07-27 22:57:55 +0600

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,34 @@
+  const { Client, ClientUser } = require('discord.js')
+/**
+ * 
+ * @param {Client} client 
+ */
+module.exports = async(client) => {
+    console.log(`${client.user.tag} Has logged in`)
+   async function ll() {
+        let totalMembers = 0;
+
+        for (const guild of client.guilds.cache) {
+            totalMembers += (await guild[1].members.fetch()).size;
+        }
+
+        var status = '$help'
+
+        const types = ['LISTENING', 'WATCHING', "STREAMING"]     
+        let randomType = types[Math.floor(Math.random() * types.length)]
+        if (randomType === 'LISTENING') status = `$help` 
+
+        if (randomType === 'STREAMING') {
+            status = `Hello!`
+        }
+
+    client.user.setPresence({activity: {type: randomType, url: "https://www.twitch.tv/", name: status}, status:  'dnd'});
+
+    //     client.user.setPresence({activity: {type: types[Math.floor(Math.random() * types.length)], url: "https://www.twitch.tv/", name: `${status[Math.floor(Math.random() * status.length)]}`}, status:  ss[Math.floor(Math.random() * ss.length)]});
+
+
+}
+setInterval(() => {
+    ll()
+}, 5000)
+}
